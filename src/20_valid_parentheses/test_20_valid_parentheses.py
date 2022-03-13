19edaf5d6ea1d56cd3275afba7557476dad15dde
nfilatov <nfilatov@wisebits.com>
2022-03-13 12:50:03 +0200

    LeetCode #20 Valid Parentheses Solution added

--- src/20_valid_parentheses/test_20_valid_parentheses.py
+++ src/20_valid_parentheses/test_20_valid_parentheses.py
@@ -1,6 +1,26 @@
-class TestSolution:
-    def test_is_valid_parentheses_true(self):
-        assert Solution().is_valid(s="(){}[]") is True
+import pytest
 
-    def test_is_valid_parentheses_false(self):
-        assert Solution().is_valid(s="(") is False
+from lc_20_valid_parentheses import Solution
+
+
+class TestSolution:
+    @pytest.mark.parametrize("valid_parentheses", [
+        "()[]{}",
+        "((([])))",
+        "((([]{})))",
+        "((([]{}[])))",
+        "[((([{()}])))]",
+    ])
+    def test_is_valid_parentheses_true(self, valid_parentheses):
+        assert Solution().is_valid(s=valid_parentheses) is True
+
+    @pytest.mark.parametrize("invalid_parentheses", [
+        "{",
+        "{}{",
+        "[({",
+        ")[(",
+        "[)][]",
+        "((([{]})))",
+    ])
+    def test_is_valid_parentheses_false(self, invalid_parentheses):
+        assert Solution().is_valid(s=invalid_parentheses) is False
